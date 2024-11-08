d1bb057b5b076bca12629c915b9705cf79aa8f6e
AhmedSleem06 <ahmed.sleem1206@gmail.com>
2024-11-08 13:28:08 +0200

    new update

--- src/EmojiPickerReact.tsx
+++ src/EmojiPickerReact.tsx
@@ -6,7 +6,6 @@ import { Body } from './components/body/Body';
 import { ElementRefContextProvider } from './components/context/ElementRefContext';
 import { PickerConfigProvider } from './components/context/PickerConfigContext';
 import { useReactionsModeState } from './components/context/PickerContext';
-
 import { Header } from './components/header/Header';
 import PickerMain from './components/main/PickerMain';
 import { compareConfig } from './config/compareConfig';
